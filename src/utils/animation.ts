88ded8214f28208097656f4bf352f20e9b444c54
Marvel-Deone <89777325+Marvel-Deone@users.noreply.github.com>
2025-03-24 16:35:25 +0100

    feat: add and complete features section

--- src/utils/animation.ts
+++ src/utils/animation.ts
@@ -1,3 +1,31 @@
+import gsap from "gsap";
+
+import { ScrollTrigger } from "gsap/all";
+gsap.registerPlugin(ScrollTrigger);
+
+interface AnimationProps {
+    [key: string]: any;
+}
+
+interface ScrollProps {
+    trigger?: string | Element;
+    toggleActions?: string;
+    start?: string;
+    [key: string]: any;
+}
+
+export const animateWithGsap = (target: string | Element, animationProps?: any, scrollProps?: ScrollProps) => {
+    gsap.to(target, {
+        ...animationProps,
+        scrollTrigger: {
+            trigger: target,
+            toggleActions: 'restart reverse restart reverse',
+            start: 'top 85%',
+            ...scrollProps
+        }
+    });
+}
+
 export const animateWithGsapTimeline = (timeline: any, rotationRef: any, rotationState: any, firstTarget: any, secondTarget: any, animationProps: any) => {
     timeline.to(rotationRef.current.rotation, {
         y: rotationState,
